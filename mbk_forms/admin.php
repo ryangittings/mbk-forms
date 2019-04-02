dbef90e870d00946e8f259d7571c14da341fc09b
Ryan Gittings <ryangittings@ryans-imac.home>
2019-04-02 17:48:58 +0100

    UPDATED: Admin

--- mbk_forms/admin.php
+++ mbk_forms/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 if ($CurrentUser->logged_in()) {
+    $this->register_app('mbk_forms', 'Perch Forms Recaptcha', 99, 'Google Recaptcha x Perch Forms', '1.0', true); 
     $this->require_version('mbk_forms', '2.0');
 
     $this->add_setting('mbk_forms_recaptcha_secret_key', 'Recaptcha secret key', 'text', '');
