0c885bf082f7c0def0e063a1a995e895baf0ad4d
Ryan Gittings <ryangittings@ryans-imac.home>
2019-04-02 15:19:01 +0100

    PATCHED: Setting name

--- mbk_forms/admin.php
+++ mbk_forms/admin.php
@@ -4,5 +4,5 @@ if ($CurrentUser->logged_in()) {
     $this->require_version('mbk_forms', '2.0');
 
     $this->add_setting('mbk_forms_recaptcha_secret_key', 'Recaptcha secret key', 'text', '');
-    $this->add_setting('mbk_forms_honeypot_name', 'Honepot field name', 'text', '');
+    $this->add_setting('mbk_forms_honeypot_name', 'Honeypot field name', 'text', '');
 }
